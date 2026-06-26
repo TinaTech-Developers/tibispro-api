6e7c13c25db19bacbcb1327f1501ad682715d03b
TinaTech-Developers <tinashephiri0@gmail.com>
2026-06-26 12:47:19 +0200

    forgot password api reset link

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -17,10 +17,17 @@ export const sendResetEmail = async (
     to,
     subject: "Password Reset Request",
     html: `
-      <h2>Password Reset</h2>
-      <p>You requested a password reset.</p>
-      <a href="${resetLink}">Click here to reset your password</a>
-      <p>If you didn't request this, ignore this email.</p>
-    `,
+    <h2>Password Reset</h2>
+
+    <p>You requested a password reset.</p>
+
+    <p>
+      <a href="${resetLink}" style="color:#2563eb; font-weight:bold;">
+        Click here to reset your password
+      </a>
+    </p>
+
+    <p>If you didn't request this, ignore this email.</p>
+  `,
   });
 };
